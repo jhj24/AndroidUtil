926359c89e9397f94e5df32bf40b7bf44f57f381
jhj24 <1406929846@qq.com>
2017-07-27 09:20:44 +0800

    Initial commit

---
+++ app/src/main/java/jhj/com/androidutil/MainActivity.java
@@ -0,0 +1,13 @@
+package jhj.com.androidutil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ library/src/main/java/jhj/com/library/DateUtil.java
@@ -0,0 +1,8 @@
+package jhj.com.library;
+
+/**
+ * Created by jhj on 17-7-27.
+ */
+
+public class DateUtil {
+}
