e6027e7b54b1abd64a4c79d0de2f9db5ce034c9a
jhj24 <1406929846@qq.com>
2017-08-01 17:58:35 +0800

    first commit

--- library/src/main/java/jhj/com/library/util/DateUtil.java
+++ library/src/main/java/jhj/com/library/util/DateUtil.java
@@ -1,8 +1,196 @@
 package jhj.com.library.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 /**
+ * 日期类处理工具
+ * <p/>
+ * Date:Tue Jul 05 11:00:33 GMT+08:00 2016
+ * Calendar:
+ * Long: 1501580257000
+ * <p/>
  * Created by jhj on 17-7-27.
  */
 
 public class DateUtil {
+
+    /**
+     * String -> long
+     *
+     * @param str     日期（eg、2016/7/5；2016/07/05 10:20等）
+     * @param pattern 日期格式(eg、yyyy/MM/dd；yyyy/MM/dd HH:mm等)
+     * @return long
+     * @throws ParseException exception
+     */
+    public static long getStringToMillis(String str, String pattern) throws ParseException {
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern, Locale.CHINA);
+        c.setTime(sdf.parse(str));
+        return c.getTimeInMillis();
+    }
+
+    /**
+     * Date -> long
+     *
+     * @param date Date
+     * @return long
+     */
+    public static long getDateToMillis(Date date) {
+        return date.getTime();
+    }
+
+
+    /**
+     * Calendar -> long
+     *
+     * @param calendar calender
+     * @return long
+     */
+    public static long getCalanderToMillis(Calendar calendar) {
+        return calendar.getTime().getTime();
+    }
+
+    /**
+     * long -> String
+     *
+     * @param millis  long
+     * @param pattern eg、"yyyy-MM-dd HH:mm"
+     * @return 日期
+     */
+    public static String milliscondToString(long millis, String pattern) {
+        Date d = new Date(millis);
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern, Locale.CHINA);
+        return sdf.format(d);
+    }
+
+    /**
+     * Date -> String
+     *
+     * @param date    Date
+     * @param pattern eg、"yyyy-MM-dd"..
+     * @return String String
+     */
+    public static String dateToString(Date date, String pattern) {
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern, Locale.CHINA);
+        return sdf.format(date);
+    }
+
+
+    /**
+     * Calender -> String
+     *
+     * @param calendar calendar
+     * @param pattern eg、"yyyy-MM-dd"..
+     * @return String
+     */
+    public static String getCalenderToString(Calendar calendar, String pattern) {
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern, Locale.CHINA);
+        return sdf.format(calendar.getTime());
+    }
+
+    /**
+     * Calender -> Date
+     *
+     * @param calendar calendar
+     * @return Date
+     */
+    public static Date getCalenderToData(Calendar calendar) {
+        return calendar.getTime();
+    }
+
+    /**
+     * long -> Date
+     *
+     * @param millis long
+     * @return Date
+     */
+    public static Date getLongToDate(long millis) {
+        return new Date(millis);
+    }
+
+    /**
+     * String -> Date
+     *
+     * @param string  eg、2017-7-25
+     * @param pattern eg、yyyy-MM-dd
+     * @return Date
+     * @throws ParseException
+     */
+    public static Date getStringToDate(String string, String pattern) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern, Locale.CHINA);
+        return sdf.parse(string);
+    }
+
+
+    /**
+     * 获取当天起始时间的毫秒值
+     *
+     * @return long
+     */
+    public static long getMillisOfToday() {
+        Calendar toDay = Calendar.getInstance();
+        return chooseDate(toDay);
+    }
+
+    /**
+     * 获取当天所在的一周的周一起始时间的毫秒值
+     *
+     * @return long
+     */
+    public static long getMillisOfWeek() {
+        Calendar thisWeek = Calendar.getInstance();
+        thisWeek.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+        return chooseDate(thisWeek);
+    }
+
+    /**
+     * 获取当天所在的月份1号起始时间的毫秒值
+     *
+     * @return long
+     */
+    public static long getMillisOfMonth() {
+        Calendar thisMonth = Calendar.getInstance();
+        thisMonth.set(Calendar.DAY_OF_MONTH, 1);
+        return chooseDate(thisMonth);
+    }
+
+    /**
+     * 设置时间为当天时间的0点0时0分0秒0毫秒
+     *
+     * @return long 毫秒值
+     */
+    private static long chooseDate(Calendar calendar) {
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar.getTime().getTime();
+    }
+
+    /**
+     * 判断两个日期是不是同一天
+     *
+     * @param d1
+     * @param d2
+     * @return boolean
+     */
+    public static Boolean isSameDay(Date d1, Date d2) {
+
+        if (null != d1 && null != d2) {
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(d1);
+            Calendar selectedDate = Calendar.getInstance();
+            selectedDate.setTime(d2);
+            return cal.get(Calendar.MONTH) == selectedDate.get(Calendar.MONTH)
+                    && cal.get(Calendar.YEAR) == selectedDate.get(Calendar.YEAR)
+                    && cal.get(Calendar.DAY_OF_MONTH) == selectedDate.get(Calendar.DAY_OF_MONTH);
+        } else {
+            return false;
+        }
+    }
+
 }
--- library/src/main/java/jhj/com/library/util/LegalityUtil.java
+++ library/src/main/java/jhj/com/library/util/LegalityUtil.java
@@ -6,6 +6,49 @@ package jhj.com.library.util;
  */
 
 public class LegalityUtil {
+
+
+    /**
+     * 正则：手机号（精确）
+     */
+    private static final String PHONE = "^1[34578]\\d{9}$";
+    /**
+     * 区号：以0开头，3位或4位
+     * 座机号：7位或8位
+     * 分机号：1到4位
+     * 正则：电话号码
+     */
+    private static final String TELEPHONE = "(0[0-9]{2,3}\\-?)?([2-9][0-9]{6,7})+(\\-[0-9]{1,4})?";
+    /**
+     * 正则：身份证号码15位
+     */
+    private static final String REGEX_ID_CARD15 = "^[1-9]\\d{7}((0\\d)|(1[0-2]))(([0|1|2]\\d)|3[0-1])\\d{3}$";
+    /**
+     * 正则：身份证号码18位
+     */
+    private static final String REGEX_ID_CARD18 = "^[1-9]\\d{5}[1-9]\\d{3}((0\\d)|(1[0-2]))(([0|1|2]\\d)|3[0-1])\\d{3}([0-9Xx])$";
+    /**
+     * 正则：邮箱
+     */
+    private static final String REGEX_EMAIL = "\\w[-\\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\\.)+[A-Za-z]{2,14}";
+    /**
+     * 正则：URL
+     */
+    private static final String REGEX_URL = "^((https|http|ftp|rtsp|mms)?:\\/\\/)[^\\s]+";
+    /**
+     * 正则：汉字
+     */
+    private static final String REGEX_ZH = "^[\\u4e00-\\u9fa5]+$";
+
+    private static boolean isMatch(String str, String regex) {
+        if (str == null || "".equals(str)) {
+            return false;
+        } else {
+            String s = str.replace(" ", "");
+            return s.matches(regex);
+        }
+    }
+
     /**
      * 验证手机号是否合法
      *
@@ -13,32 +56,67 @@ public class LegalityUtil {
      * @return boolean
      */
     public static boolean isPhoneNum(String num) {
-        if (num == null || "".equals(num)) {
-            return false;
-        } else {
-            String s = num.replace(" ", "");
-            String regex = "[1][34578]\\d{9}";
-            return s.matches(regex);
-        }
+        return isMatch(num, PHONE);
     }
 
     /**
-     * 验证固话是否合法
-     * 区号：以0开头，3位或4位
-     * 座机号：7位或8位
-     * 分机号：1到4位
+     * 验证固话合法性
      *
      * @param num num
      * @return boolean
      */
     public static boolean isTelephoneNum(String num) {
-        if (num == null || "".equals(num)) {
-            return false;
-        } else {
-            String s = num.replace(" ", "");
-            String regex = "(0[0-9]{2,3}\\-?)?([2-9][0-9]{6,7})+(\\-[0-9]{1,4})?";
-            return s.matches(regex);
-        }
+        return isMatch(num, TELEPHONE);
+    }
+
+    /**
+     * 验证15位省份证合法性
+     *
+     * @param num num
+     * @return boolean
+     */
+    public static boolean isIdCard15(String num) {
+        return isMatch(num, REGEX_ID_CARD15);
+    }
+
+    /**
+     * 验证15位省份证合法性
+     *
+     * @param num num
+     * @return boolean
+     */
+    public static boolean isIdCard18(String num) {
+        return isMatch(num, REGEX_ID_CARD18);
+    }
+
+    /**
+     * 验证邮箱
+     *
+     * @param num num
+     * @return boolean
+     */
+    public static boolean isEmail(String num) {
+        return isMatch(num, REGEX_EMAIL);
+    }
+
+    /**
+     * 验证URL
+     *
+     * @param num 待验证文本
+     * @return boolean
+     */
+    public static boolean isURL(String num) {
+        return isMatch(num, REGEX_URL);
+    }
+
+    /**
+     * 验证汉字
+     *
+     * @param str string
+     * @return boolean
+     */
+    public static boolean isChinese(String str) {
+        return isMatch(str, REGEX_ZH);
     }
 
 
